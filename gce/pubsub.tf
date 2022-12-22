8e9677e2e6fc72ff3d9261d7050fe3ddcc92649d
vsm <devops.july2.2017@gmail.com>
2022-12-23 02:55:23 +0530

    Create pubsub.tf

---
+++ gce/pubsub.tf
@@ -0,0 +1,22 @@
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+}
+
+resource "google_pubsub_subscription" "example" {
+  name  = "example-subscription"
+  topic = google_pubsub_topic.example.name
+
+  ack_deadline_seconds = 20
+
+  labels = {
+    foo = "bar"
+  }
+
+  push_config {
+    push_endpoint = "https://example.com/push"
+
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+}
